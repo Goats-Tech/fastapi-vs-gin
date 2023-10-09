ab573744effdda11f7ca0a0dbb990ef69d57a56b
Fernando Arteaga <fernan2ac11@gmail.com>
2023-10-08 23:43:30 -0400

    revert sessionmaker

--- fast_api/src/db/__init__.py
+++ fast_api/src/db/__init__.py
@@ -1,5 +1,4 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 from src.config import DatabaseConfig
 
@@ -11,11 +10,8 @@ db_url = f"postgresql+psycopg://{conn_string}"
 
 db_engine = create_engine(db_url)
 
-DBSession = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
 
 def db_connection():
-    session = DBSession()
-    try:
-        yield session
-    finally:
-        session.close()
+    conn = db_engine.engine.connect()
+    yield conn
+    conn.close()
